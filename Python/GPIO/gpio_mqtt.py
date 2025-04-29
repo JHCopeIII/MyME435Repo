691f552cf18ceecd14d1400981bad0a94596e864
JHCopeIII <copejh@rose-hulman.edu>
2025-04-29 17:01:25 -0400

    Update

--- Python/GPIO/gpio_mqtt.py
+++ Python/GPIO/gpio_mqtt.py
@@ -1,9 +1,17 @@
 from mqtt_helper import MqttClient
+import gpiozero as gz
 import time
 
-class APP:
+class App:
     def __init__(self):
         self.red_led = gz.LED(14)
+        self.yellow_led = gz.LED(15)
+        self.green_led = gz.LED(18)
+
+        self.button_23 = gz.Button(23)
+        self.button_23.when_pressed = self.button_23_pressed
+        self.button_25 = gz.Button(25)
+        self.button_25.when_pressed = lambda: self.mqtt_client.send_message("reset")
 
         self.mqtt_client = MqttClient()
         self.mqtt_client.callback = self.mqtt_callback
@@ -11,8 +19,14 @@ class APP:
                                 publish_topic_name="me435/copejh/to_computer",
                                 use_off_campus_broker=False)
 
+        def button_23_pressed(self):
+            print("Button 23 pressed")
+            self.mqtt_client.send_message("button_23")
+        
+
         def mqtt_callback(self, type_name, payload):
             print(f"My callback: {type_name} - {payload}")
+
             if type_name == "red":
                 if payload == "on":
                     self.red_led.on()
@@ -20,8 +34,38 @@ class APP:
                     self.red_led.off()
                 else:
                     print("Unknown payload for red LED:", payload)
-            else:
-                print("Unknown type: {payload}")
+            
+            if type_name == "yellow":
+                if payload == "on":
+                    self.yellow_led.on()
+                elif payload == "off":
+                    self.yellow_led.off()
+                else:
+                    print("Unknown payload for yellow LED:", payload)
+
+            if type_name == "green":
+                if payload == "on":
+                    self.green_led.on()
+                elif payload == "off":
+                    self.green_led.off()
+                else:
+                    print("Unknown payload for green LED:", payload)
+
+            if type_name == "leds":
+                if payload[0] == 0:
+                    self.red_led.off()
+                elif payload[0] == 1:
+                    self.red_led.on()
+
+                if payload[1] == 0:
+                    self.yellow_led.off()
+                elif payload[1] == 1:
+                    self.yellow_led.on()
+
+                if payload[2] == 0:
+                    self.green_led.off()
+                elif payload[2] == 1:
+                    self.green_led.on()
         
 
 def main():
@@ -32,9 +76,15 @@ def main():
         while True:
             time.sleep(0.1)
             #app.mqtt_client.send_message("red", "on")
-            #time.sleep(2.0)
+            #app.mqtt_client.send_message("yellow", 1)
+            #app.mqtt_client.send_message("green", True)
+            app.mqtt_client.send_message("leds", [1, 0, 1])
+            time.sleep(2.0)
             #app.mqtt_client.send_message("red", "off")
-            #time.sleep(2.0)
+            #app.mqtt_client.send_message("yellow", 0)
+            #app.mqtt_client.send_message("green", False)
+            app.mqtt_client.send_message("leds", [0, 1, 0])
+            time.sleep(2.0)
 
 
     except KeyboardInterrupt:
