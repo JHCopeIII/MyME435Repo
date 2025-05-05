9802bd9a71e27156e3afc3c8652479d87daa75f7
JHCopeIII <copejh@rose-hulman.edu>
2025-05-05 10:37:39 -0400

    Update

--- RoseBot/rosebot_drive_system.py
+++ RoseBot/rosebot_drive_system.py
@@ -15,6 +15,13 @@ class DriveSystem:
         right_duty = self.scale_speed(right_speed)
         self.car.set_motor_model(left_duty, left_duty, right_duty, right_duty)
 
+    def go(self, left_speed, right_speed):
+        left_duty = self.scale_speed(left_speed)
+        right_duty = self.scale_speed(right_speed)
+        self.car.set_motor_model(left_duty, left_duty, right_duty, right_duty)
+
+    def stop(self)
+        self.set_speeds(0,0,0,0)
 
     def go_straight_for_seconds(self, seconds, speed):
         self.go(speed, speed)
